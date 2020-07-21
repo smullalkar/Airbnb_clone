1c9304939de8fad1223d9699c37a8203f4ec03ea
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-07-21 16:18:05 +0530

    Front-end folder structure is ready

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src='./dummy.png'></img>
+    </div>
+  );
+}
+
+export default App;
