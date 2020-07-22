396b55f70df8854f0919d6ee8a18a63afc228db1
Shreedhar Bhat <shridharbhat1998@gmail.com>
2020-07-22 17:52:15 +0530

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Footer from './Components/Common/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
