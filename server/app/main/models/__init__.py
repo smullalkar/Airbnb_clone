920fe4e245d68a38dbdab587d9e3217bcf74db71
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-07-22 18:29:26 +0530

    UserModel done

--- server/app/main/models/__init__.py
+++ server/app/main/models/__init__.py
@@ -3,7 +3,3 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 from .UserModel import UserModel
-from .PropertyModel import PropertyModel
-from .RentModel import RentModel
-from .MessageModel import MessageModel
-from .ClosureModel import ClosureModel
