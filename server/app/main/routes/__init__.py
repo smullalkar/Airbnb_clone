8df2b3f761e81db989ae7bb245a4c7ce9d8702a2
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-07-22 18:29:54 +0530

    authentication routes done

--- server/app/main/routes/__init__.py
+++ server/app/main/routes/__init__.py
@@ -1,7 +1,5 @@
 from flask import Blueprint
 
 user = Blueprint('user', __name__)
-owner = Blueprint('owner',__name__)
-message = Blueprint('message',__name__)
 
-from . import User,Owner,Message
+from . import User
