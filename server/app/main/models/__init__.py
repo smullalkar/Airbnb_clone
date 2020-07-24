211e7b8d5bd5c5032ac2270d07cc82b352ba8667
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-07-24 13:01:55 +0530

    Tables updated

--- server/app/main/models/__init__.py
+++ server/app/main/models/__init__.py
@@ -3,3 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 from .UserModel import UserModel
+from .UserOAuthModel import UserOAuthModel
+from .CountriesModel import CountriesModel
+from .StateModel import StateModel
+from .CityModel import CityModel
+from .AmenitiesModel import AmenitiesModel
+from .CategoriesModel import CategoriesModel
+from .CityModel import CityModel
+from .CountriesModel import CountriesModel
+from .CurrencyModel import CurrencyModel
+from .MinimumStayModel import MinimumStayModel
+from .PriceTypeModel import PriceTypeModel
+from .PropertyModel import PropertyModel
+from .PropertyTypeModel import PropertyTypeModel
+from .PropertyImagesModel import PropertyImagesModel
+from .StateModel import StateModel
