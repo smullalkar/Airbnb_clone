6d5f6806dd0d233db7969457af19073b73cfc289
Shreedhar Bhat <shridharbhat1998@gmail.com>
2020-07-24 12:58:51 +0530

    reducers updated

--- client/src/Redux/authentication/reducer.js
+++ client/src/Redux/authentication/reducer.js
@@ -17,13 +17,24 @@ import {
     GOOGLE_LOGIN_REQUEST,
     GOOGLE_LOGIN_SUCCESS,
     GOOGLE_LOGIN_FAILURE,
+    CLOSE_LOGIN_MODAL,
+    CLOSE_REGISTER_MODAL
 } from "./actionTypes";
 
 
+function getUserInfo() {
+    let token = localStorage.getItem("token");
+    if (!token ) {
+        token = "";
+    }
+    return [token ];
+}
+
+const [token] = getUserInfo();
 
 const initState = {
+    token,
     name: "",
-    email: "",
     accessToken: "",
     provider: "",
     id: "",
@@ -31,7 +42,9 @@ const initState = {
     errorMessage: "",
     isLoading: true,
     isAuth: false,
-    payload : ""
+    payload: "",
+    isShowLoginModal: false,
+    isShowRegisterModal: false
 }
 
 const reducer = (state = initState, { type, payload }) => {
@@ -132,6 +145,40 @@ const reducer = (state = initState, { type, payload }) => {
                 error: true,
                 errorMessage: "something went wrong"
             }
+
+        case LOGOUT_USER_REQUEST:
+            return {
+                ...state,
+                error: false,
+                errorType: "",
+                errorMessage: ""
+            };
+        case LOGOUT_USER_SUCCESS:
+            localStorage.setItem("token", "");
+            return {
+                ...state,
+                isAuth: false,
+                token: "",
+                userType: "",
+            };
+        case LOGOUT_USER_FAILURE:
+            return {
+                ...state,
+                error: true,
+                errorType: "logout",
+                errorMessage: "logout failed"
+            };
+        case CLOSE_LOGIN_MODAL:
+            console.log("invoked")
+            return {
+                ...state,
+                isShowLoginModal: !state.isShowLoginModal
+            }
+            case CLOSE_REGISTER_MODAL :
+                return{
+                    ...state,
+                    isShowRegisterModal : !state.isShowRegisterModal
+                }
         default:
             return state
     }
