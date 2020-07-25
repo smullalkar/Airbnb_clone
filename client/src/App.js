b7b971dce5c9dea87645968209882176ba461438
Shreedhar Bhat <shridharbhat1998@gmail.com>
2020-07-25 11:09:10 +0530

    Added the routes in the main app

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,14 @@ import './bootstrap-override.css'
 import NavBar from './Components/Common/NavBar/NavBar'
 import Home from './Components/Common/Home/Home'
 import Footer from './Components/Common/Footer/Footer'
-
+import Routes from "./Components/Common/Route/Routes"
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Routes />
 
-      <Home />
-
-      <Footer />
     </div>
   );
 }
