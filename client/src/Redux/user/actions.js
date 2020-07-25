34a45d550a88aea31eeb8427d1bd2c47edcc4fec
Shreedhar Bhat <shridharbhat1998@gmail.com>
2020-07-25 11:10:45 +0530

    redux updated to get all data from backend and loading state added

--- client/src/Redux/user/actions.js
+++ client/src/Redux/user/actions.js
@@ -1,5 +1,15 @@
-import { GET_DATA_REQUEST, GET_DATA_SUCCESS, GET_DATA_FAILURE } from "./actionTypes";
+import {
+    GET_DATA_REQUEST,
+    GET_DATA_SUCCESS,
+    GET_DATA_FAILURE,
+    GET_DATA_PAGEWISE_REQUEST,
+    GET_DATA_PAGEWISE_SUCCESS,
+    GET_DATA_PAGEWISE_FAILURE
+} from "./actionTypes";
+import axios from "axios";
 
+
+// Actions to get Data from home page search start
 export const getDataRequest = () => ({
     type: GET_DATA_REQUEST
 });
@@ -18,11 +28,40 @@ export const getData = payload => {
     return dispatch => {
         dispatch(getDataRequest());
         return axios
-            .get("/user/login", {
-            })
+            .get("/user/searchresults", payload)
             .then(res => {
                 dispatch(getDataSuccess(res.data));
             })
             .catch(() => dispatch(getDataFailure()));
     };
 }
+// Actions to get Data from home page search end
+
+// Actions to get Data pagewise(pagination) start
+// export const getDataRequest = () => ({
+//     type: GET_DATA_REQUEST
+// });
+
+// export const getDataSuccess = payload => ({
+//     type: GET_DATA_SUCCESS,
+//     payload
+// });
+
+// export const getDataFailure = () => ({
+//     type: GET_DATA_FAILURE
+// });
+
+// export const getData = payload => {
+//     console.log(payload)
+//     return dispatch => {
+//         dispatch(getDataRequest());
+//         return axios
+//             .get("/user/searchresults", payload)
+//             .then(res => {
+//                 dispatch(getDataSuccess(res.data));
+//             })
+//             .catch(() => dispatch(getDataFailure()));
+//     };
+// }
+// Actions to get Data pagewise(pagination) end
+
--- client/src/Redux/user/reducer.js
+++ client/src/Redux/user/reducer.js
@@ -1,23 +1,27 @@
 import { GET_DATA_REQUEST, GET_DATA_SUCCESS, GET_DATA_FAILURE } from "./actionTypes";
-import reducer from "../authentication/reducer";
 
 const initState = {
-
+    data: [],
+    isLodaing: false
 }
 
 const userReducer = (state = initState, { type, payload }) => {
     switch (type) {
         case GET_DATA_REQUEST:
             return {
-                ...state
+                ...state,
+                isLodaing: true
             }
         case GET_DATA_SUCCESS:
             return {
-                ...state
+                ...state,
+                data: [...state.data, payload],
+                isLodaing: false
             }
         case GET_DATA_FAILURE:
             return {
-                ...state
+                ...state,
+                isLodaing: false
             }
         default:
             return { ...state }
