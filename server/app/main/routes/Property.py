bd5d591cff851b5d06872b31010b60dd0622c641
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-07-27 09:44:06 +0530

    More Routes to filters and search added

---
+++ server/app/main/routes/Property.py
@@ -0,0 +1,105 @@
+from . import user
+from flask import request
+from ..services.filters import sendAmenities, sendFacility, sendPropertyType, sendCategories
+from ..services.searchs import user_search_results, search
+from ..services.review import sendReviews
+from ..services.booking import add_booking, send_booking
+from ..services.recommendation import recommendation
+import datetime
+# from ..util.auth_token import checkAuthToken
+
+@user.route("/search")
+def usearch():
+    """
+    sending user search results
+    """
+    params = request.args
+    response = search(params)
+    return response
+
+
+@user.route("/searchresults")
+def user_search():
+    """
+    sending user search results
+    """
+    params = request.args
+    print('params....',params)
+    response = user_search_results(params)
+    return response
+
+@user.route("/amenities")
+def send_amenities():
+    """
+    sending amenities
+    """
+    response = sendAmenities()
+    return response
+
+@user.route("/facilities")
+def send_facility():
+    """
+    sending facilities
+    """
+    response = sendFacility()
+    return response
+
+@user.route("/propertytype")
+def send_propertytype():
+    """
+    sending property type
+    """
+    response = sendPropertyType()
+    return response
+
+@user.route("/categories")
+def send_categories():
+    """
+    sending categories
+    """
+    response = sendCategories()
+    return response
+
+@user.route("/reviews", methods=["POST"])
+def send_reviews():
+    """
+    sending reviews
+    """
+    response = sendReviews(request.json)
+    return response
+
+
+@user.route("/addbooking", methods=["POST"])
+def addbook():
+    """
+    add booking done by user
+    """
+    response = add_booking(request.json)
+    return response
+
+@user.route("/sendbooking", methods=["POST"])
+def sendbook():
+    """
+    send booking data of the property
+    """
+    response = send_booking(request.json)
+    return response
+
+
+@user.route("/recommendation", methods=["POST"])
+def recom():
+    """
+    sending recommendation based on property viewed by user
+    """
+    params = request.args
+    print('params....',params)
+    response = recommendation(params,request.json)
+    return response
+
+
+# @user.route("/token_validate", methods=["POST"])
+# def token_validate():
+
+#     response = checkAuthToken(request.json)
+
+#     return response
