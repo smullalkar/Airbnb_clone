fad412f020a6b705a375ecf6825059684e911b36
Shreedhar Bhat <shridharbhat1998@gmail.com>
2020-07-27 21:21:51 +0530

    dynamic routing for entity page

--- client/src/Components/Common/Route/Routes.jsx
+++ client/src/Components/Common/Route/Routes.jsx
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "../Home/Home";
 import Listing from "../Listing/Listing";
+import Entity from "../Entity/Entity"
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/:url/entity/:param" render={()=><Entity/>} />
         <Route
           exact
           path="/:urlParams"
