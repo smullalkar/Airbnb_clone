fb89d5481d0d3d578a6fafbe1ef5f2239308884f
Shreedhar Bhat <shridharbhat1998@gmail.com>
2020-07-27 11:38:01 +0530

    route updated for entity page

--- client/src/Components/Common/Route/Routes.jsx
+++ client/src/Components/Common/Route/Routes.jsx
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "../Home/Home";
 import Listing from "../Listing/Listing";
+import Entity from "../Entity/Entity";
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route
+          exact
+          path="/entity/entity_page"
+          render={(props) => <Entity {...props} />}
+        />
         <Route
           exact
           path="/:urlParams"
