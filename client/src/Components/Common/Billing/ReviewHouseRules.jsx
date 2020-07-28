27f02ff3e2970b562ff18b2773c6a273c9b45cfa
Abhinaya-bala <abhinayafeb19@gmail.com>
2020-07-28 10:22:29 +0530

    Billing page created

---
+++ client/src/Components/Common/Billing/ReviewHouseRules.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+import { } from 'react-bootstrap'
+
+
+class ReviewHouseRules extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+    render() {
+        const { } = this.props
+        return (
+
+            <div>
+                <Navbar bg="light" expand="lg">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="">1. Review house rules</Nav.Link>
+                        <Nav.Link href="">&#8250;</Nav.Link>
+                        <Nav.Link href="">2. Who’s coming?</Nav.Link>
+                        <Nav.Link href="">&#8250;</Nav.Link>
+                        <Nav.Link href="">3. Confirm and pay</Nav.Link>
+                    </Nav>
+
+                </Navbar>
+
+
+
+
+            </div>
+        );
+    }
+}
+
+
+
+
+export default ReviewHouseRules;
