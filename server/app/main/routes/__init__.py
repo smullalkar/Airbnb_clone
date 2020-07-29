0ffee26213c19350a432f7d38378e4b4e93118a8
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-07-29 15:58:09 +0530

    routes added

--- server/app/main/routes/__init__.py
+++ server/app/main/routes/__init__.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 user = Blueprint('user', __name__)
 owner = Blueprint('owner',__name__)
-# payment = Blueprint('payment',__name__)
+payment = Blueprint('payment',__name__)
 
-from . import User, Property, Owner
+from . import User, Property, Owner, Payment
