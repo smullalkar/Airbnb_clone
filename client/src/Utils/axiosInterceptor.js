5aea35973d4abea96e526539012c8d83825ca788
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-07-30 13:29:59 +0530

    updated utils

--- client/src/Utils/axiosInterceptor.js
+++ client/src/Utils/axiosInterceptor.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const axiosInstance = axios.create({
-  baseURL: "http://f6770beacf21.ngrok.io/"
+  baseURL: "http://127.0.0.1:5000/"
 });
 
 axiosInstance.interceptors.request.use((request) => {
