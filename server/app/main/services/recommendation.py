2daf120e30edc2c198947a9e50492660e37a869d
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-08-02 23:38:11 +0530

    fixed bug and slight change in the algorithm

--- server/app/main/services/recommendation.py
+++ server/app/main/services/recommendation.py
@@ -6,6 +6,7 @@ import datetime
 import time
 from ..util.auth_token import checkAuthToken
 from .searchs import sendData
+import math
 
 
 def recommendation(params):
@@ -14,34 +15,32 @@ def recommendation(params):
     """
     try:
         location = params.get('location')
-        cityId = params.get('city_id', default=0)
-        stateId = params.get('state_id', default=0)
-        countryId = params.get('country_id', default=0)
+        cityId = int(params.get('city_id', default=0))
+        stateId = int(params.get('state_id', default=0))
+        countryId = int(params.get('country_id', default=0))
         checkin = params.get('checkin',datetime.date.today() + datetime.timedelta(days=1))
         checkout = params.get('checkout',datetime.date.today() + datetime.timedelta(days=2))
-        children = params.get('children', default=0)
-        infants = params.get('infants', default=0)
-        adults = params.get('adults', default=0)
-        perPage = params.get('per_page', default=20)
+        children = int(params.get('children', default=0))
+        infants = int(params.get('infants', default=0))
+        adults = int(params.get('adults', default=0))
+        perPage = int(params.get('per_page', default=20))
         totalguests = int(adults)+int(children)
         print('................',location)
 
-        category = params.getlist('type_of_place')
+        category = params.get('type_of_place')
         isCancel = params.get('is_cancel')
         instantBook = params.get('instantBook')
-        minPrice = params.get('minPrice', default=0)
-        maxPrice = params.get('maxPrice', default=0)
-        bed = params.get('bed', default=0)
-        bedrooms = params.get('bedrooms', default=0)
-        bathrooms = params.get('bathrooms', default=0)
-        amenities = params.getlist('amenities')
-        propertyType = params.getlist('property_type')
-        facility = params.getlist('facility')
-        print('................',amenities)
+        minPrice = int(params.get('minPrice', default=0))
+        maxPrice = int(params.get('maxPrice', default=0))
+        bed = int(params.get('bed', default=0))
+        bedrooms = int(params.get('bedrooms', default=0))
+        bathrooms = int(params.get('bathrooms', default=0))
+        amenities = params.get('amenities')
+        propertyType = params.get('property_type')
+        facility = params.get('facilities')
+        print('................amenities',amenities)
         print('................f',facility)
 
-        propertyId = params.get('property_id',default=0)
-        
         if maxPrice <= 1000:
             minPrice = 0
             maxPrice = maxPrice
@@ -59,9 +58,9 @@ def recommendation(params):
             maxPrice = maxPrice + (maxPrice * 0.30)
 
         else:
-            minPrice = maxPrice - (maxPrice * 0.20)
-            maxPrice = maxPrice + (maxPrice * 0.20)
-            
+            minPrice = maxPrice - (maxPrice * 0.25)
+            maxPrice = maxPrice + (maxPrice * 0.25)
+
     except KeyError as err:
         return json.dumps({'error': True, 'error_found': format(err)})
     except TypeError as err:
@@ -93,24 +92,11 @@ def recommendation(params):
                 JOIN facility ON facility_property.facilityId=facility.id
                 JOIN images ON p.id=images.propertyId
                 JOIN categories AS cct ON p.categoryId=cct.id 
-                JOIN review as r ON r.propertyId=p.id WHERE '''
-
-    if location is not None:
-        query = query + ' c.cityName = "%s" AND '%(location)
-
-    if totalguests != 0:
-        query = query + ' p.accomodatesCount >= %d AND '%(totalguests)
-
-    if category is not None and len(category) != 0:
-        if len(category) > 1:
-            for i in category:
-                query = query + ' ct.categoryName = "%s" OR '%(i)
-        else:
-            query = query + ' ct.categoryName = "%s" AND '%(category[0])
-
+                JOIN review as r ON r.propertyId=p.id WHERE c.cityName = "%s" AND '''%(location)
+                
     if isCancel is not None:
-        query = query + ' p.isCancel = {0} AND '.format(isCancel)
-        
+            query = query + ' p.isCancel = {0} AND '.format(isCancel)
+            
     if instantBook is not None:
         query = query + ' p.instantBook = {0} AND '.format(instantBook)
 
@@ -126,46 +112,259 @@ def recommendation(params):
     if bathrooms != 0:
         query = query + ' p.bathroomCount = %d AND '%(bathrooms)
 
-    if amenities is not None and len(amenities) != 0:
-        if len(amenities) > 1:
-            if len(amenities) <= 3: 
-                for i in range (len(amenities)-1):
-                    query = query + ' amenities.aminityName = "%s" OR '%(amenites[i])
-            elif len(amenities) > 3 and len(amenities) <=6:
-                for i in range(3):
-                    query = query + ' amenities.aminityName = "%s" OR '%(amenites[i])
-            elif len(amenities) > 6:
-                for i in range(5):
-                    query = query + ' amenities.aminityName = "%s" OR '%(amenites[i])
-        else:
-            query = query + ' amenities.aminityName = "%s" AND '%(amenities[0])
-
-    if facility is not None and len(facility) != 0 :
-        if len(facility) > 1:
-            for i in facility:
-                query = query + ' facility.facilityName = "%s" OR '%(i)
-        else:
-            query = query + ' facility.facilityName = "%s" AND '%(facility[0])
-
-    if propertyType is not None and len(propertyType) != 0:
-        if len(propertyType) > 1:
-            for i in propertyType:
-                query = query + ' ppt.propertyType = "%s" OR '%(i)
-        else:
-            query = query + ' ppt.propertyType = "%s" AND '%(propertyType[0])
-
+    if amenities is not None or facility is not None:
+        query = query.strip('AND ')
+        query = query + ' GROUP BY p.id ;'
+        results = db.session.execute(query)
+        
+        similarData = []
+        for element in results:
+            amen = element.amenities
+            fac = element.facilities
+            
+            a = amen.split(',') #amenities of the enity got from db
+            f = fac.split(',') #facilities got from db
+                    
+            if amenities is not None and facility is None:
+                amenities_ = amenities.split(",") #amenities to filter
+                similarity = math.ceil(len(amenities_)*0.6)
+                match = 0
+                flag = False
+                for i in range(len(a)):
+                    for j in range(len(amenities_)):
+                        searchAmenity = amenities_[j].split('_')
+                        searchAmenity = " ".join(searchAmenity)
+                        if a[i] == searchAmenity:
+                            match+=1
+                            if match == similarity:
+                                flag=True
+                                break
+                if flag:
+                    similarData.append(element)
+                            
+            if amenities is None and facility is not None:
+                facility_ = facility.split(",") #facility to filter
+                similarity = math.ceil(len(facility_)*0.6)
+                match = 0
+                flag = False
+                for i in range(len(f)):
+                    for j in range(len(facility_)):
+                        searchFacility = facility_[j].split('_')
+                        searchFacility = " ".join(searchFacility)
+                        if f[i] == searchFacility:
+                            match+=1
+                            if match == similarity:
+                                flag=True
+                                break
+                if flag:
+                    similarData.append(element)
+            
+            if amenities is not None and facility is not None:
+                amenities_ = amenities.split(",") #amenities to filter
+                facility_ = facility.split(",") #facility to filter
+                similarityAmenity = math.ceil(len(amenities_)*0.6)
+                similarityFacility = math.ceil(len(facility_)*0.6)
+                match1 = 0
+                match2 = 0
+                flag1 = False
+                flag2 = False
+                
+                for i in range(len(a)):
+                    for j in range(len(amenities_)):
+                        searchAmenity = amenities_[j].split('_')
+                        searchAmenity = " ".join(searchAmenity)
+                        if a[i] == searchAmenity:
+                            match1+=1
+                            if match1 == similarityAmenity:
+                                flag1=True
+                                break
+                
+                for m in range(len(f)):
+                    for n in range(len(facility_)):
+                        searchFacility = facility_[j].split('_')
+                        searchFacility = " ".join(searchFacility)
+                        if f[m] == searchFacility:
+                            match2+=1
+                            if match2 == similarityFacility:
+                                flag2=True
+                                break
+                if flag1 and flag2:
+                    similarData.append(element)
+            
+        d = sendData(similarData)
+        print('filtered data',d)
+        return json.dumps({
+            "data": d,
+            "message": 'Successful',
+            'error': False
+        }, default=str)
+        
     query = query.strip('AND ')
-    query = query.strip('OR ')
-    query = query + ' AND p.id != %d GROUP BY p.id ;'%(int(propertyId))
+    query = query + ' GROUP BY p.id ;'
     results = db.session.execute(query)
 
     d = sendData(results)
-    print(d)
+    print('data',d)
     return json.dumps({
         "data": d,
         "message": 'Successful',
         'error': False
     }, default=str)
+
+    # try:
+    #     location = params.get('location')
+    #     cityId = params.get('city_id', default=0)
+    #     stateId = params.get('state_id', default=0)
+    #     countryId = params.get('country_id', default=0)
+    #     checkin = params.get('checkin',datetime.date.today() + datetime.timedelta(days=1))
+    #     checkout = params.get('checkout',datetime.date.today() + datetime.timedelta(days=2))
+    #     children = params.get('children', default=0)
+    #     infants = params.get('infants', default=0)
+    #     adults = params.get('adults', default=0)
+    #     perPage = params.get('per_page', default=20)
+    #     totalguests = int(adults)+int(children)
+    #     print('................',location)
+
+    #     category = params.getlist('type_of_place')
+    #     isCancel = params.get('is_cancel')
+    #     instantBook = params.get('instantBook')
+    #     minPrice = int(params.get('minPrice', default=0))
+    #     maxPrice = int(params.get('maxPrice', default=0))
+    #     bed = params.get('bed', default=0)
+    #     bedrooms = params.get('bedrooms', default=0)
+    #     bathrooms = params.get('bathrooms', default=0)
+    #     amenities = params.getlist('amenities')
+    #     propertyType = params.getlist('property_type')
+    #     facility = params.getlist('facility')
+    #     print('................am',amenities)
+    #     print('................f',facility)
+
+    #     propertyId = params.get('property_id',default=0)
+        
+    #     if maxPrice <= 1000:
+    #         minPrice = 0
+    #         maxPrice = maxPrice
+
+    #     elif maxPrice > 1000 and maxPrice <= 3000:
+    #         minPrice = maxPrice - (maxPrice * 0.50)
+    #         maxPrice = maxPrice + (maxPrice * 0.50)
+
+    #     elif maxPrice > 3000 and maxPrice <= 5000:
+    #         minPrice = maxPrice - (maxPrice * 0.40)
+    #         maxPrice = maxPrice + (maxPrice * 0.40)
+
+    #     elif maxPrice > 5000 and maxPrice < 10000:
+    #         minPrice = maxPrice - (maxPrice * 0.30)
+    #         maxPrice = maxPrice + (maxPrice * 0.30)
+
+    #     else:
+    #         minPrice = maxPrice - (maxPrice * 0.20)
+    #         maxPrice = maxPrice + (maxPrice * 0.20)
+            
+    # except KeyError as err:
+    #     return json.dumps({'error': True, 'error_found': format(err)})
+    # except TypeError as err:
+    #     return json.dumps({'error': True, 'error_found': format(err)})
+    # except Exception as err:
+    #     return json.dumps({'error': True, 'error_found': format(err)})
+
+    # # SELECT * FROM property AS p JOIN city_model AS c ON p.cityId=c.id JOIN property_type AS pt ON p.propertyTypeId=pt.id JOIN categories AS ct ON p.categoryId=ct.id JOIN state_model as st on c.stateId=st.id JOIN countries_model AS cm ON st.countryId=cm.id WHERE c.cityName='Bengaluru';        
+
+    # query = ''' SELECT p.id,p.propertyName,p.description,p.address,
+    #             p.countryId,cm.countryName,p.stateId,st.stateName,
+    #             ppt.propertyType,ct.categoryName,c.cityName,p.istantBook,p.isCancel,
+    #             p.refundType,p.price,p.accomodatesCount,p.lat,p.lng,
+    #             p.bathroomCount,p.isAvailable,p.bedCount,p.bedroomCount,
+    #             p.cityId,p.userId, AVG(r.rating) AS rating, COUNT(r.rating) AS ratingcount,
+    #             GROUP_CONCAT(DISTINCT amenities.aminityName) AS amenities,
+    #             GROUP_CONCAT(DISTINCT facility.facilityName) AS facilities,
+    #             GROUP_CONCAT(DISTINCT images.image) AS images
+    #             FROM property AS p 
+    #             JOIN city_model AS c ON p.cityId=c.id 
+    #             JOIN property_type AS pt ON p.propertyTypeId=pt.id 
+    #             JOIN categories AS ct ON p.categoryId=ct.id 
+    #             JOIN state_model as st on c.stateId=st.id 
+    #             JOIN countries_model AS cm ON st.countryId=cm.id 
+    #             JOIN property_type AS ppt on p.propertyTypeId=ppt.id 
+    #             JOIN amenities_property ON p.id=amenities_property.propertyId 
+    #             JOIN amenities ON amenities_property.amenityId=amenities.id
+    #             JOIN facility_property ON p.id=facility_property.propertyId 
+    #             JOIN facility ON facility_property.facilityId=facility.id
+    #             JOIN images ON p.id=images.propertyId
+    #             JOIN categories AS cct ON p.categoryId=cct.id 
+    #             JOIN review as r ON r.propertyId=p.id WHERE '''
+
+    # if location is not None:
+    #     query = query + ' c.cityName = "%s" AND '%(location)
+
+    # if totalguests != 0:
+    #     query = query + ' p.accomodatesCount >= %d AND '%(totalguests)
+
+    # if category is not None and len(category) != 0:
+    #     if len(category) > 1:
+    #         for i in category:
+    #             query = query + ' ct.categoryName = "%s" OR '%(i)
+    #     else:
+    #         query = query + ' ct.categoryName = "%s" AND '%(category[0])
+
+    # if isCancel is not None:
+    #     query = query + ' p.isCancel = {0} AND '.format(isCancel)
+        
+    # if instantBook is not None:
+    #     query = query + ' p.instantBook = {0} AND '.format(instantBook)
+
+    # if maxPrice != 0 and minPrice != 0:
+    #     query = query + ' p.price >= %d AND p.price <= %d AND '%(int(minPrice),int(maxPrice))
+
+    # if bed != 0:
+    #     query = query + ' p.bedCount = %d AND '%(bed)
+
+    # if bedrooms != 0:
+    #     query = query + ' p.bedroomCount = %d AND '%(bedrooms)
+
+    # if bathrooms != 0:
+    #     query = query + ' p.bathroomCount = %d AND '%(bathrooms)
+
+    # if amenities is not None and len(amenities) != 0:
+    #     if len(amenities) > 1:
+    #         if len(amenities) <= 3: 
+    #             for i in range (len(amenities)-1):
+    #                 query = query + ' amenities.aminityName = "%s" AND '%(amenites[i])
+    #         elif len(amenities) > 3 and len(amenities) <=6:
+    #             for i in range(3):
+    #                 query = query + ' amenities.aminityName = "%s" AND '%(amenites[i])
+    #         elif len(amenities) > 6:
+    #             for i in range(5):
+    #                 query = query + ' amenities.aminityName = "%s" AND '%(amenites[i])
+    #     else:
+    #         query = query + ' amenities.aminityName = "%s" AND '%(amenities[0])
+
+    # if facility is not None and len(facility) != 0 :
+    #     if len(facility) > 1:
+    #         for i in facility:
+    #             query = query + ' facility.facilityName = "%s" OR '%(i)
+    #     else:
+    #         query = query + ' facility.facilityName = "%s" AND '%(facility[0])
+
+    # if propertyType is not None and len(propertyType) != 0:
+    #     if len(propertyType) > 1:
+    #         for i in propertyType:
+    #             query = query + ' ppt.propertyType = "%s" OR '%(i)
+    #     else:
+    #         query = query + ' ppt.propertyType = "%s" AND '%(propertyType[0])
+
+    # query = query.strip('AND ')
+    # query = query.strip('OR ')
+    # query = query + ' AND p.id != %d GROUP BY p.id ;'%(int(propertyId))
+    # results = db.session.execute(query)
+
+    # d = sendData(results)
+    # print(d)
+    # return json.dumps({
+    #     "data": d,
+    #     "message": 'Successful',
+    #     'error': False
+    # }, default=str)
 
 
 def recommendation_popularity(params):
