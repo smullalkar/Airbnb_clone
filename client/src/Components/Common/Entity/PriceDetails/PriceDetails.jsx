5f0feae2235b0ae36ac17e7374646e5471720e64
Shreedhar Bhat <shridharbhat1998@gmail.com>
2020-08-02 19:57:40 +0530

    warning for date

--- client/src/Components/Common/Entity/PriceDetails/PriceDetails.jsx
+++ client/src/Components/Common/Entity/PriceDetails/PriceDetails.jsx
@@ -237,7 +237,22 @@ class PriceDetails extends Component {
             </Card>
             <Row>
               <Col>
-                {showWarning ? "Sorry !! These dates are not available" : null}{" "}
+                {showWarning ? <span style={{ color: "red", fontSize: 15, padding:10, textAlign:"center" }}>
+                      <svg
+                        style={{ width: 20 }}
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                      </svg>{" "}
+                      <span className="mr-2">
+                        Sorry !! These dates are not available
+                      </span>
+                    </span> : null}{" "}
               </Col>
             </Row>
             <Button
